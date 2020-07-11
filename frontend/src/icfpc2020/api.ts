ba99ee3dc8320bdba688f5d6f2d194d3d679a468
garasubo <garasubo@gmail.com>
2020-07-11 16:27:20 +0900

    update solutions components

--- frontend/src/icfpc2020/api.ts
+++ frontend/src/icfpc2020/api.ts
@@ -1,7 +1,12 @@
 import {Problem} from './problems/Problems';
+import {Solution} from './solutions/Solutions';
 
 export const fecthProblems = (page: number) => {
     return fetch(process.env.REACT_APP_API_URL + "v1/icfpc2019/problems?page=" + page)
         .then((response) => response.json() as Promise<Problem[]>);
 };
 
+export const fetchSolutions = (page: number) => {
+    return fetch(process.env.REACT_APP_API_URL + "v1/icfpc2019/solutions?page=" + page)
+        .then((response) => response.json() as Promise<Solution[]>);
+}
